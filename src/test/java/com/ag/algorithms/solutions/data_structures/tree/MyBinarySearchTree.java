8931b572039ebeb090da0a78d2b3dca73086ebf1
Attila Gyori <attila.gyori.id@gmail.com>
2021-08-17 07:43:44 +0200

    binary search tree

---
+++ src/test/java/com/ag/algorithms/solutions/data_structures/tree/MyBinarySearchTree.java
@@ -0,0 +1,19 @@
+package com.ag.algorithms.solutions.data_structures.tree;
+
+public class MyBinarySearchTree {
+    private Node root;
+
+    public MyBinarySearchTree() {
+        this.root = null;
+    }
+
+    public Node insert(Integer value) {
+
+        return null;
+    }
+
+    public Node lookUp(Integer value) {
+
+        return null;
+    }
+}
