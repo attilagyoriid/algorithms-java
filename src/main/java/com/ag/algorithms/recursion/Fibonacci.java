dde712b5bb3cbd2d56e890f8d3739c545a010804
Attila Gyori <attila.gyori.id@gmail.com>
2021-12-18 13:06:50 +0100

    Fibonacci iterative and recursive

--- src/main/java/com/ag/algorithms/recursion/Fibonacci.java
+++ src/main/java/com/ag/algorithms/recursion/Fibonacci.java
@@ -1,6 +1,11 @@
 package com.ag.algorithms.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibonacci {
+
+    private final Map<Integer, Integer> cache = new HashMap<>();
 
     public int calculate(int num) {
 
@@ -11,6 +16,8 @@ public class Fibonacci {
         return calculate(num - 1) + calculate(num - 2);
 
     }
+
+
 
     public int calculateIteratively(int num) {
 
